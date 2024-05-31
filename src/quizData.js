829f4a8c48a2ce17f45b5cc5f7504c87fbd3d988
vikrantdabhi-devitpl <137795121+vikrantdabhi-devitpl@users.noreply.github.com>
2024-05-31 11:40:29 +0530

    Update quizData.js
    Added new question

--- src/quizData.js
+++ src/quizData.js
@@ -1,5 +1,10 @@
 const quizData = [
     {
+      question: "What was the first video game ever made?",
+      options: ["Pong", "Spacewar!", "Tetris", "Computer Space"],
+      answer: "Spacewar!"
+    },
+    {
       question: "Which company developed the first commercial antivirus software?",
       options: ["Symantec", "McAfee", "Norton", "Kaspersky Lab"],
       answer: "McAfee"
